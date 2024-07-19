3035361e192664fdf9cfa6c5446d994e27c32ab4
eptakam <emmataks@gmail.com>
2024-07-19 11:01:55 -0300

    A closer look at 'getStaticPros' and configuration options.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import path from 'path';  // path est un module node.js qui permet de manipuler des chemins de fichiers
 import fs from 'fs/promises';  // fs (file system) est un module node.js qui permet de lire des fichiers cote serveur
+import { notFound } from 'next/navigation';
 
 export default function HomePage(props) {
   const { products } = props;
@@ -20,7 +21,8 @@ export default function HomePage(props) {
 
 // faire un static generation avant que ce composant soit rendu pour justement avoir les données avant le rendu car contrairement a l'approche 'App Router' dans laquelle les composant sont des 'server components' et donc les données sont chargées au moment du rendu, avec l'appproche 'Pages Router', il faut dire a next.js de faire un 'static generation' avant le rendu du composant pour avoir les données avant le rendu.
 // ceci sera execute seulement au moment de la generation de la page
-export async function getStaticProps() {
+// elle peut prendre un parametre 'context' qui contient des informations sur la requete
+export async function getStaticProps(context) {
   // Note: vu que getStaticProps est exécuté côté serveur, on peut lire directement des fichiers system (cote serveur) avec fs etant dans notre composant sans avoir besoin de les importer
   // fs.readFileSync() lit le contenu d'un fichier
   // cwd (current working directory) est le répertoire de travail courant
@@ -28,10 +30,24 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);  // JSON.parse() convertit une chaîne JSON en objet JavaScript
 
+  if(!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };  // si le tableau de produits est vide, renvoyer une page 404
+  }
+
   return {
     props: {
       products: data.products,  // data est un objet qui contient un tableau de produits
     },
     revalidate: 10,  // revalidate est une option qui permet de regénérer la page après 10 secondes. c'est ce qu'on appelle l'incremental static regeneration (ISR)
+    //notFound: true,  // notFound est une option qui permet de renvoyer une page 404 si les données ne sont pas trouvées
+    //redirect: {  // redirect est une option qui permet de rediriger l'utilisateur vers une autre page si les données ne sont pas trouvées
   };
 }
